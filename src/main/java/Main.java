99705eba1daba02b5dbd6baa93cbbbeb0acfd4e5
Roman Karpiak <roman.karpiak@gmail.com>
2021-05-06 01:21:55 +0400

    Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
-import com.roman.Twitter;
+import com.roman.Quoter;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-          context.getBean(Twitter.class).sayQuote();
-
+        context.getBean(Quoter.class).sayQuote();
     }
 }
